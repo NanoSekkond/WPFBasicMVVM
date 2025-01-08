17bfef9f62d4cb1861d9d74de4294b13855cfca6
Nano <mudkip2435@gmail.com>
2025-01-08 20:28:53 -0300

    First commit of WPF basic library

---
+++ RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace WPFBasicMVVM
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void Execute(object? parameter) => _execute();
+        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute();
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ TitleBar.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPFBasicMVVM
+{
+    public partial class TitleBarView : UserControl
+    {
+        private bool _isDragging = false;
+        private Point _startMousePosition;
+
+        public TitleBarView()
+        {
+            InitializeComponent();
+        }
+
+        private void DragMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _isDragging = true;
+                _startMousePosition = e.GetPosition(this);
+                DragButton.CaptureMouse();
+            }
+        }
+
+        private void DragMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDragging)
+            {
+                Window parentWindow = Window.GetWindow(this);
+                Point currentMousePosition = e.GetPosition(this);
+                Vector delta = currentMousePosition - _startMousePosition;
+
+                parentWindow.Left += delta.X;
+                parentWindow.Top += delta.Y;
+            }
+        }
+
+        private void DragMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDragging)
+            {
+                _isDragging = false;
+                DragButton.ReleaseMouseCapture();
+            }
+        }
+
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.Close();
+        }
+    }
+}
